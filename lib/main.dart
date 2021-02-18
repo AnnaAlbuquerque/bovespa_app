000fd6514888e6f759e52658ac05dc314a967941
Anna Albuquerque <anna.b.albuquerque@gmail.com>
2021-02-17 22:31:21 -0300

    Takes off degub banner

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:bovespa_app/symbols_page.dart';
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primaryColor: Color.fromRGBO(18, 101, 113, 1.0),
